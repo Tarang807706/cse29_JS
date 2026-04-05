f631f3bf869e99a89a9d3f41b95a10fa5a7a7931
Tarang807706 <mittaltarang584@gmail.com>
2026-04-06 01:51:10 +0530

    exp3

---
+++ class.js
@@ -0,0 +1,99 @@
+
+//  const x=document.getElementById("p1");
+
+
+//     // console.log(x);
+// x.innerHTML="<br>tarang mittal</br>";
+// const result=document.getElementsByClassName("p1");
+
+// console.log(result);
+
+// result[1].innerHTML="mittal";
+// result[0].innerHTML="tarang";
+
+// const result=document.getElementsByTagName("p3");
+// result[2] Style.color="blue";
+// result[1]innerHTML="this is a get"
+// result[2] Style.color="blue";
+
+
+
+
+// const result=document.querySelector("p1");
+// const result=document.querySelectorAll("p1");
+
+
+
+
+
+
+// const x=document.getElementById("tm").getAttribute("class");
+// document.getElementById("d").innerHTML=x;
+
+
+
+// create element.....
+// const heading=document.createElement("h2");
+// const bodying=document.getElementsByTagName("body");
+// bodytag[0].append(heading);
+
+
+// events in javascript
+// mouse event
+// onclick=
+// onmouseover=
+// onmouseout=
+// onmousedown=
+// onmouseup=
+// onmousemove=
+// ondblclick=
+
+
+
+// form events
+// onfocus
+// onsubmit
+// onblur
+// onchange
+
+// x.addEventListener("focus");
+// x.addEventListener("blur");
+
+// window event
+// onload
+// onunload
+// onresize
+
+
+// JSON
+// javascript object Notation 
+
+
+
+// var text={'employes'}+{'firstname="jon", lastname="don"'}+
+// {'firstname "mr", lastname="smith"'}+{'firstname="tarang", lastname="mittal"'};
+
+// filter() function
+// the Filter() method creates a new array with elements that satisfy a given condition.
+
+
+
+
+// asynchronous programming
+// // unblocking code
+// let promise= new promise((resolve,reject)=>(let success =true;))
+
+
+// promise.then
+
+
+
+// save permanent
+// localStorage.setitem("name","jeet");
+// remove
+// localStorage.removeItem("name","jeet");
+
+// object ko string me 
+// localStorage.setItem("user",JSON.stringify(user));
+
+// var data =JSON.parse(localStorage)
